31853a590e8466fe3eac44a49d370b45b1fc0f96
Flavia Regina <flaviafreire23@gmail.com>
2022-05-18 14:08:54 -0300

    Início da construção do sistema de getão de base de dados

--- core/classes/database.php
+++ core/classes/database.php
@@ -2,11 +2,73 @@
 
 namespace core\classes;
 
+use PDO;
+use PDOException;
+
 class Database
 {
 
-    public function __construct()
+    private $ligacao;
+
+    //========================================
+    private function ligar()
     {
-        echo 'Base de dados!';
+        // ligar à base de dados
+        $this->ligacao = new PDO(
+            'mysql:' .
+                'host=' . MYSQL_SERVER . ';' .
+                'dbname=' . MYSQL_DATABASE . ';' .
+                'charset=' . MYSQL_CHARSET,
+            MYSQL_USER,
+            MYSQL_PASS,
+            array(PDO::ATTR_PERSISTENT => true)
+        );
+
+        //debug
+        $this->ligacao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    }
+
+    //=========================================
+    private function desligar()
+    {
+        // desliga-se da base de dados
+        $this->ligacao = null;
+    }
+
+    //=========================================
+    //CRUD
+    //=========================================
+    public function select($sql, $parametros = null)
+    {
+
+        // executa função de pesquisa de SQL
+        $this->ligar();
+
+        $resultados = null;
+
+        // comunicar
+        try {
+
+            // comunicação com a bd
+            if (!empty($parametros)) {
+                $executar = $this->ligacao->prepare($sql);
+                $executar->execute($parametros);
+                $resultados = $executar->fetchAll(PDO::FETCH_CLASS);
+            } else {
+                $executar = $this->ligacao->prepare($sql);
+                $executar->execute($parametros);
+                $resultados = $executar->fetchAll(PDO::FETCH_CLASS);
+            }
+        } catch (PDOException $e) {
+
+            // caso exista erro
+            return false;
+        }
+
+        // desligar da bd
+        $this->desligar();
+
+        // devolver os resultados obtidos
+        return $resultados;
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,9 +2,6 @@
 
 // abrie a sessao
 
-use core\classes\Database;
-use core\classes\Functions;
-
 session_start();
 
 // carregar o config
@@ -12,19 +9,3 @@ require_once('../config.php');
 
 // carrega todas as classes do projeto
 require_once('../vendor/autoload.php');
-
-$a = new Database();
-$b = new Functions();
-
-$b->teste();
-
-echo 'ok';
-
-/*
-carregar o config
-carregar classes 
-carregar o sistema de rotas
-    -mostrar loja
-    -mostrar carrinho
-    -mostrar o backoffice, etc 
-*/
