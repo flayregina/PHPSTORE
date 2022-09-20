96182674f3e4dbaaaaa250eff88d8fc93110c82b
Flavia Regina <flaviafreire23@gmail.com>
2022-09-20 16:46:22 -0300

    Arranjo visual e rotas de navegação

--- core/controladores/Main.php
+++ core/controladores/Main.php
@@ -10,21 +10,41 @@ class Main
     public function index()
     {
 
-        $dados = [
-            'titulo' => APP_NAME . ' ' . APP_VERSION,
-        ];
-
         Store::Layout([
             'layouts/html_header',
             'layouts/header',
             'inicio',
             'layouts/footer',
             'layouts/html_footer',
-        ], $dados);
+        ]);
     }
     //=========================================================
     public function loja()
+
     {
-        echo 'Loja!!!!';
+        //Apresenta a página da loja
+
+        Store::Layout([
+            'layouts/html_header',
+            'layouts/header',
+            'loja',
+            'layouts/footer',
+            'layouts/html_footer',
+        ]);
+    }
+
+    //=============================================================
+    public function carrinho()
+
+    {
+        //Apresenta a página do carrinho
+
+        Store::Layout([
+            'layouts/html_header',
+            'layouts/header',
+            'carrinho',
+            'layouts/footer',
+            'layouts/html_footer',
+        ]);
     }
 }
